11fa6942d897897401db9f582a39e465ce5ce3bd
Barani Htut <barani.t16@gmail.com>
2019-10-26 19:46:52 -0700

    Submit

--- schema.sql
+++ schema.sql
@@ -1,71 +1,43 @@
-SELECT  emp.emp_no,
-        emp.last_name,
-        emp.first_name,
-        emp.gender,
-        sal.salary
-FROM employees as emp
-    LEFT JOIN salaries as sal
-    ON (emp.emp_no = sal.emp_no)
-ORDER BY emp.emp_no;
+CREATE TABLE employees (
+    emp_no      INT             NULL,
+    birth_date  DATE            NOT NULL,
+    first_name  VARCHAR(15)     NOT NULL,
+    last_name   VARCHAR(15)     NOT NULL,
+    gender      VARCHAR(15) 		NOT NULL,
+    hire_date   DATE            NOT NULL,
+);
 
-SELECT first_name, last_name
-FROM employees
-WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+CREATE TABLE departments (
+    dept_no     VARCHAR(15)         NOT NULL,
+    dept_name   VARCHAR(15)        NOT NULL,
+);
 
-SELECT  dm.dept_no,
-        d.dept_name,
-        dm.emp_no,
-        e.last_name,
-        e.first_name,
-        dm.from_date,
-        dm.to_date
-FROM dept_manager AS dm
-    INNER JOIN departments AS d
-        ON (dm.dept_no = d.dept_no)
-    INNER JOIN employees AS e
-        ON (dm.emp_no = e.emp_no);
+CREATE TABLE dept_manager (
+   dept_no      VARCHAR(15)         NOT NULL,
+   emp_no       INT             NOT NULL,
+   from_date    DATE            NOT NULL,
+   to_date      DATE            NOT NULL,
+);
 
-SELECT  e.emp_no,
-        e.last_name,
-        e.first_name,
-        d.dept_name
-FROM employees AS e
-    INNER JOIN dept_emp AS de
-        ON (e.emp_no = de.emp_no)
-    INNER JOIN departments AS d
-        ON (de.dept_no = d.dept_no)
-ORDER BY e.emp_no;
+CREATE TABLE dept_emp (
+    emp_no      INT             NOT NULL,
+    dept_no     VARCHAR(15)         NOT NULL,
+    from_date   DATE            NOT NULL,
+    to_date     DATE            NOT NULL,
+    FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+);
 
-SELECT *
-FROM employees
-WHERE first_name = 'Hercules'
-AND last_name LIKE 'B%';
+CREATE TABLE titles (
+    emp_no      INT             NOT NULL,
+    title       VARCHAR(15)     NOT NULL,
+    from_date   DATE            NOT NULL,
+    to_date     DATE,
+);
 
-SELECT  e.emp_no,
-        e.last_name,
-        e.first_name,
-        d.dept_name
-FROM employees AS e
-    INNER JOIN dept_emp AS de
-        ON (e.emp_no = de.emp_no)
-    INNER JOIN departments AS d
-        ON (de.dept_no = d.dept_no)
-WHERE d.dept_name = 'Sales'
-ORDER BY e.emp_no;
-
-SELECT  e.emp_no,
-        e.last_name,
-        e.first_name,
-        d.dept_name
-FROM employees AS e
-    INNER JOIN dept_emp AS de
-        ON (e.emp_no = de.emp_no)
-    INNER JOIN departments AS d
-        ON (de.dept_no = d.dept_no)
-WHERE d.dept_name IN ('Sales', 'Development')
-ORDER BY e.emp_no;
-
-SELECT last_name, COUNT(last_name)
-FROM employees
-GROUP BY last_name
-ORDER BY COUNT(last_name) DESC;
+CREATE TABLE salaries (
+    emp_no      INT             NOT NULL,
+    salary      INT             NOT NULL,
+    from_date   DATE            NOT NULL,
+    to_date     DATE            NOT NULL,
+);
